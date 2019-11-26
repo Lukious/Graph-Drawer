a9d48a15fa60930c0284c14e67e1fd22ee000851
Lukious <zhsjzhsj@gmail.com>
2019-11-26 16:16:04 +0900

    Temping

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import PIL
 from PIL import Image
 import pandas as pd
 import numpy as np
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
 """
 im2 = Image.new("RGB", (500,500), (200,200,200)) 
@@ -32,10 +33,27 @@ def generator(score):
         row1 = Image.new("RGB", (100,50),(200,200,200))
         backg.paste(row1, (20,(i*100)+t_placer))      
         
+    t_placer +=125
+        
     for i in range(int(len(score.index)/2)):    
         ypos = ((b_placer - t_placer)/len(score.index))*i
         row2 = Image.new("RGB", (100,50),(200,200,200))
-        backg.paste(row2, (175,(i*100)+t_placer))      
+#        backg.text((225, (i*125)+t_placer), "something123")
+        backg.paste(row2, (225,(i*125)+t_placer))      
+        
+    t_placer +=75
+        
+    for i in range(int(len(score.index)/4)+1):    
+        ypos = ((b_placer - t_placer)/len(score.index))*i
+        row3 = Image.new("RGB", (100,50),(200,200,200))
+        backg.paste(row3, (425,(i*125)+t_placer))      
+        
+    t_placer +=70
+        
+    for i in range(int(len(score.index)/4)):    
+        ypos = ((b_placer - t_placer)/len(score.index))*i
+        row3 = Image.new("RGB", (100,50),(200,200,200))
+        backg.paste(row3, (625,(i*125)+t_placer))     
         
     backg.save("result.png")
     
